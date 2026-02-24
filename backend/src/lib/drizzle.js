1a12fec166cfb1ea8e0915c9b5548296fe2fb678
Renzify <168178279+Renzify@users.noreply.github.com>
2026-02-24 16:40:01 +0800

    delete drizzle api

--- backend/src/lib/drizzle.js
+++
@@ -1,7 +0,0 @@
-import { drizzle } from "drizzle-orm/neon-serverless";
-import { neon } from "@neondatabase/serverless";
-import { ENV } from "./env.js";
-import * as schema from "../schemas/schema.js";
-
-const sql = neon(ENV.DATABASE_URL);
-export const db = drizzle(sql, { schema });
