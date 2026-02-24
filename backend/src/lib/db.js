7ab1e661c269304e4ffca447c4329a2cd1996165
Manuel <barcelonamanuelmarc@gmail.com>
2026-02-24 14:43:01 +0800

    feat: add local database

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -10,4 +10,13 @@ const pool = new Pool({
   ssl: { rejectUnauthorized: false },
 });
 
+pool.on("connect", () => {
+  console.log("Connected to PostgreSQL database");
+});
+
+pool.on("error", (err) => {
+  console.error("Unexpected database error", err);
+  process.exit(-1);
+});
+
 export default pool;
